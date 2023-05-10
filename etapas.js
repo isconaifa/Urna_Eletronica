954c1bf7f6aa5c9ae4c570d3bc67769060512843
isconaifagit config --global user.name isconaifagit config --global user.name isconaifa <iscobasdostdbv@gmail.com>
2023-05-10 18:03:28 -0400

    Continuação do desenvolvimento da urna eletrônica

---
+++ etapas.js
@@ -0,0 +1,16 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            numero: 38111,
+            nome: "Fulano de tal",
+            partido: "ABC",
+            fotos:[
+                {url: "./images/trincao.peg"
+                }
+            ]
+
+        ]
+    }
+]; 
